ef74399c1ac227f463a39551934f2f5ac5d82cba
rafael-navarro03 <faeledu108@gmail.com>
2025-04-22 16:51:00 -0300

    API Finalizada
    + Testes da IA

--- AnimalDex/Projeto - AnimalDex/venv/Scripts/activate.bat
+++ AnimalDex/Projeto - AnimalDex/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Senac\7° Semestre\Design de Aplicativos\Animal_Dex\Animal_Dex\AnimalDex\Projeto - AnimalDex\venv
+set VIRTUAL_ENV=C:\Users\Rafa\Documents\GitHub\Animal_Dex\AnimalDex\Projeto - AnimalDex\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
